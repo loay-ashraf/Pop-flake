016b63da5b870c9f5eb7482eba3f5b4463b4f005
Loay Ashraf <loay.ashraf.96@gmail.com>
2022-02-08 20:40:44 +0200

    feat: create JSON Codable models
    Resolves: none.

---
+++ Pop-flake/Modules/Main/Tab Bar/Home/Model/HomeModel.swift
@@ -0,0 +1,105 @@
+//
+//  HomeModel.swift
+//  Pop-flake
+//
+//  Created by Loay Ashraf on 08/02/2022.
+//
+
+import Foundation
+
+// MARK: - MostPopular
+struct MostPopular: Codable {
+    let items: [TopRatedMovie]
+    let errorMessage: String
+}
+
+struct ComingSoon: Codable {
+    let items: [FullMovie]
+    let errorMessage: String
+}
+
+struct InTheaters: Codable {
+    let items: [FullMovie]
+    let errorMessage: String
+}
+
+struct TopRated: Codable {
+    let items: [TopRatedMovie]
+    let errorMessage: String
+}
+
+struct BoxOffice: Codable {
+    let items: [BoxOfficeMovie]
+    let errorMessage: String
+}
+
+// MARK: - Movie
+struct FullMovie: Codable {
+    let id, title, fullTitle, year: String
+    let releaseState: String
+    let image: URL
+    let runtimeMins, runtimeStr, plot, contentRating: String
+    let imDBRating, imDBRatingCount, metacriticRating, genres: String
+    let genreList: [GenreList]
+    let directors: String
+    let directorList: [RList]
+    let stars: String
+    let starList: [RList]
+
+    enum CodingKeys: String, CodingKey {
+        case id, title, fullTitle, year, releaseState, image, runtimeMins, runtimeStr, plot, contentRating
+        case imDBRating = "imDbRating"
+        case imDBRatingCount = "imDbRatingCount"
+        case metacriticRating, genres, genreList, directors, directorList, stars, starList
+    }
+}
+
+// MARK: - RList
+struct RList: Codable {
+    let id, name: String
+}
+
+// MARK: - GenreList
+struct GenreList: Codable {
+    let key, value: String
+}
+
+// MARK: - TopRatedMovie
+struct TopRatedMovie: Codable {
+    let id, rank, title, fullTitle: String
+    let year: String
+    let image: URL
+    let crew, imDBRating, imDBRatingCount: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, rank, title, fullTitle, year, image, crew
+        case imDBRating = "imDbRating"
+        case imDBRatingCount = "imDbRatingCount"
+    }
+}
+
+// MARK: - Item
+struct BoxOfficeMovie: Codable {
+    let id, rank, title: String
+    let image: URL
+    let weekend, gross, weeks: String
+}
+
+// MARK: - MovieTrailer
+struct MovieTrailer: Codable {
+    let imDBID, title, fullTitle, type: String
+    let year, videoID, videoTitle, videoDescription: String
+    let thumbnailURL: URL
+    let uploadDate: String?
+    let link, linkEmbed: URL
+    let errorMessage: String
+
+    enum CodingKeys: String, CodingKey {
+        case imDBID = "imDbId"
+        case title, fullTitle, type, year
+        case videoID = "videoId"
+        case videoTitle, videoDescription
+        case thumbnailURL = "thumbnailUrl"
+        case uploadDate, link, linkEmbed, errorMessage
+    }
+}
---
+++ Pop-flake/Modules/Main/Tab Bar/Search/Model/SearchModel.swift
@@ -0,0 +1,27 @@
+//
+//  SearchModel.swift
+//  Pop-flake
+//
+//  Created by Loay Ashraf on 08/02/2022.
+//
+
+import Foundation
+
+// MARK: - Search
+struct Search: Codable {
+    let searchType, expression: String
+    let results: [SearchResult]
+    let errorMessage: String
+}
+
+// MARK: - Result
+struct SearchResult: Codable {
+    let id, resultType: String
+    let image: URL
+    let title, resultDescription: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, resultType, image, title
+        case resultDescription = "description"
+    }
+}
